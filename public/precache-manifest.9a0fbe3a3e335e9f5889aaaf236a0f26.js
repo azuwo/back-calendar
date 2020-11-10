0529542c193166cb20044fb33e4410daa3fcdedd
git <azuwo.dev@gmail.com>
2020-11-10 17:08:31 +0100

    v0.1 Calerdar

---
+++ public/precache-manifest.9a0fbe3a3e335e9f5889aaaf236a0f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec80ecbe6d2e103c1cffa4de9076e030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c078f7a945f95cbee92",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "39ffa6501d977e033713",
+    "url": "/static/css/main.a16ad6ab.chunk.css"
+  },
+  {
+    "revision": "7c078f7a945f95cbee92",
+    "url": "/static/js/2.80200397.chunk.js"
+  },
+  {
+    "revision": "d2f96996f8124c202886cf057702599d",
+    "url": "/static/js/2.80200397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ffa6501d977e033713",
+    "url": "/static/js/main.2dd67c5e.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
