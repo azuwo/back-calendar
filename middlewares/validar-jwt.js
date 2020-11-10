fd92dcfc6658e3069c44f27f706d2dbcad164076
git <azuwo.dev@gmail.com>
2020-11-10 11:32:28 +0100

    fix on validar-jwt

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,39 +1,33 @@
-const {response} = require('express')
-const jwt = require('jsonwebtoken')
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
 
-const validarJWT = (req,res=response,next) => {
+const validarJWT = (req, res = response, next) => {
+  //x-token headers
+  const token = req.header("x-token");
 
-    //x-token headers
-    const token = req.header('x-token')
+  // console.log(token)
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "no token in get",
+    });
+  }
 
-    // console.log(token)
-    if(!token){
-        return req.status(401).json({
-            ok:false,
-            msg:'no token in get'
-        })
-    }
+  try {
+    const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
 
-    try {
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Invalid token",
+    });
+  }
 
-        const {uid,name} = jwt.verify(
-            token,
-            process.env.SECRET_JWT_SEED,
-        )
+  next();
+};
 
-        req.uid = uid
-        req.name = name
-
-    } catch (error) {
-        return res.status(401).json({
-            ok:false,
-            msg:'Invalid token'
-        })
-    }
-
-    next()
-}
-
-module.exports ={
-    validarJWT
-}
+module.exports = {
+  validarJWT,
+};
